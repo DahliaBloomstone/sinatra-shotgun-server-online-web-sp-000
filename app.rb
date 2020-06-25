5df74b242a436fefd83a58188a0addb967e67163
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-25 21:04:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! poopoooo!!!! I BUILT THIS! "
+    "Welcome to your app!!!! WOW!!!!!! SHOTGUN SERVER I BUILT THIS! "
   end
 
 end
